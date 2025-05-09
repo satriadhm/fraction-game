1a1edd1c20e1a211a9533de0baaac653d9051c88
satriadhm <glorioussatria@gmail.com>
2025-05-09 21:26:12 +0700

    fix

--- src/app/components/organisms/SwipableContent.tsx
+++ src/app/components/organisms/SwipableContent.tsx
@@ -102,6 +102,19 @@ const SwipableContent: React.FC<SwipableContentProps> = ({
     purple: "bg-purple-500 hover:bg-purple-600",
     orange: "bg-orange-500 hover:bg-orange-600",
     yellow: "bg-yellow-500 hover:bg-yellow-600",
+    red: "bg-red-500 hover:bg-red-600",
+    gray: "bg-gray-500 hover:bg-gray-600",
+    indigo: "bg-indigo-500 hover:bg-indigo-600",
+    teal: "bg-teal-500 hover:bg-teal-600",
+    lime: "bg-lime-500 hover:bg-lime-600",
+    emerald: "bg-emerald-500 hover:bg-emerald-600",
+    rose: "bg-rose-500 hover:bg-rose-600",
+    fuchsia: "bg-fuchsia-500 hover:bg-fuchsia-600",
+    violet: "bg-violet-500 hover:bg-violet-600",
+    sky: "bg-sky-500 hover:bg-sky-600",
+    amber: "bg-amber-500 hover:bg-amber-600",
+    cyan: "bg-cyan-500 hover:bg-cyan-600",
+    zinc: "bg-zinc-500 hover:bg-zinc-600",
   }[buttonColor];
 
   // Button animations
